33c0a48442001ae5a468b9f44000f8c18e0017fb
Owen Trueblood <jmptable@mit.edu>
2014-08-27 18:08:18 -0400

    Run vlla-js async.

--- src/demotime/handler.clj
+++ src/demotime/handler.clj
@@ -4,11 +4,37 @@
         [clojure.java.shell :only [sh]])
   (:require [compojure.core :refer :all]
             [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [clojure.core.async :as async]))
 
 (def top-wrapper "try {")
 (def bottom-wrapper "} catch(e) { print(e); }")
 
+(defn js-alive?
+  [proc]
+  (try (do (.exitValue proc) false)
+       (catch IllegalThreadStateException e true)))
+
+(defn exec-js
+  [filename]
+  (let [cmd (str "/home/owen/projects/vlla-js/bin/vlla-js " filename)]
+    (do
+      (println (str "executing js from " filename))
+      (.exec (Runtime/getRuntime) cmd))))
+
+(def js-chan (async/chan))
+
+(async/go
+  (loop [js-proc nil]
+    (let [filename (async/<! js-chan)]
+      (if (not (nil? js-proc))
+        (do
+          (if (js-alive? js-proc)
+            (do
+              (println "destroying old js process")
+              (.destroy js-proc)))))
+      (recur (exec-js filename)))))
+    
 (defroutes app-routes
   (GET "/" [] (editor-page))
   (POST "/demo" req
@@ -19,8 +45,8 @@
           (do
             (with-open [wrtr (writer filename)]
               (.write wrtr (str top-wrapper code bottom-wrapper)))
-            (println (str "wrote to " filename))
-            (:out (sh "/home/owen/projects/vlla-js/bin/vlla-js" filename)))))
+            (async/>!! js-chan filename)
+            "success")))
   (route/resources "/")
   (route/not-found "Not Found"))
 
